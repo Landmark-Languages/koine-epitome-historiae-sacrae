ec835a4abd86b1191f5b2bd4b2df341f207510d9
Jason Rogers <jasonmrog@gmail.com>
2024-08-31 11:40:21 -0400

    updates for building

---
+++ scripts/md-to-html.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path from "path";
+import { parseArgs } from "node:util";
+import markdownIt from "markdown-it";
+import markdownItFootnote from "markdown-it-footnote";
+
+const md = markdownIt().use(markdownItFootnote);
+
+const { values } = parseArgs({
+  options: {
+    input: {
+      type: "string",
+      short: "i",
+    },
+    output: {
+      type: "string",
+      short: "o",
+    },
+  },
+});
+const { input, output } = values;
+const inputPath = path.isAbsolute(input) ? input : path.resolve(__dirname, input);
+const outputPath = path.isAbsolute(output) ? output : path.resolve(__dirname, output);
+
+fs.writeFileSync(outputPath, md.render(fs.readFileSync(inputPath).toString()));
